a70672e64a079f12044966cbbc118afe2906b13b
hrudaniyal <niyaljohn@gmail.com>
2025-03-14 16:25:02 +0530

    findme

---
+++ findme.py
@@ -0,0 +1,10 @@
+code = input('enter code :')
+names = ['alok','hruda']
+def findme(name):
+    if name in names:
+        print(f'i know you are {name}')
+    else :
+        print("i don't know")
+        
+
+findme(code)
