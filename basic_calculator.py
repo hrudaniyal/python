d7c6df7618226b52ebcb92e2722d72c0f28b3745
hrudaniyal <niyaljohn@gmail.com>
2025-04-08 18:13:40 +0530

    basic calculator

---
+++ basic_calculator.py
@@ -0,0 +1,10 @@
+val1 = int(input('Enter First Number :: '))
+val2 = int(input('Enter Second Number ::'))
+Add = val1+val2
+Sub = val2-val1
+Mul = val1 * val2
+Div = val1 / val2
+print(f'Addition : {Add}')
+print(f'Subtraction : {Sub}')
+print(f'Multiplication : {Mul}')
+print(f'Division : {Div}')
