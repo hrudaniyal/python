cb2403a19a6c9a96732230c326fc17356285cec3
hrudaniyal <niyaljohn@gmail.com>
2025-04-09 16:01:52 +0530

    use basic math module

---
+++ using_math.py
@@ -0,0 +1,9 @@
+import math 
+num = int(input('enter a number : '))
+squareroot = math.sqrt(num)
+log = math.log(num)
+sine = math.sin(num)
+print(f'square root is : {squareroot}')
+print(f'logarithm is   : {log}')
+print(f'sine is        : {sine}')
+
