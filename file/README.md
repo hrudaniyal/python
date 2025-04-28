9b5a5da14a8f9c6e6f804acfb3f15961d5c71341
hrudaniyal <niyaljohn@gmail.com>
2025-04-28 07:53:34 +0530

    append file

---
+++ file/README.md
@@ -0,0 +1,11 @@
+## Task 1: Read File Content
+
+- Opens a file `sample.txt`
+- Prints its content line by line
+- Handles error if the file is missing
+
+## Task 2: Write and Append Data to a File
+
+- Takes user input and writes to `output.txt`
+- Appends more data to the same file
+- Displays the final file content
--- file/addfile.py
+++ file/addfile.py
@@ -1,10 +1,20 @@
-def addfile(msg):
-    file = open('./file/output.txt','a')
-    file.write(msg)
-    print('file append sucess !!')
+user_input = input("Enter some data to write to the file: ")
+try:
+    with open('./file/output.txt', 'w') as file:
+        file.write(user_input + '\n')
+except:
+    print("file not found ")
+additional_input = input("Enter additional data to append to the file: ")
+try:
+    with open('./file/output.txt', 'a') as file:
+        file.write(additional_input + '\n')
+except:
+    print("file not found ")
 
-u_input = input('write text for add in file : ')
-try :
-    addfile(u_input)
-except :
-    print('file not found')
+print("\nFinal content of the file:")
+try:
+    with open('./file/output.txt', 'r') as file:
+        content = file.read()
+        print(content)
+except:
+    print("file not found ")
